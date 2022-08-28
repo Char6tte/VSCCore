0e4e2ba2701d12d92425b6fc3c729329d73490a4
BuildTools <unconfigured@null.spigotmc.org>
2022-08-28 10:07:00 +0900

    #1

---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.net.URI
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+    application
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+group = "com.charlotte04"
+version = "1.0-SNAPSHOT"
+java.sourceCompatibility=JavaVersion.VERSION_17
+val mcVersion = "1.19.2"
+
+repositories {
+    mavenCentral()
+    maven {
+        url = URI("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    }
+    maven{
+        url = URI("https://papermc.io/repo/repository/maven-public/")
+    }
+    maven{
+        url = URI("https://oss.sonatype.org/content/groups/public/")
+    }
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    // Align versions of all Kotlin components
+    //implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+
+    // Kotlin
+    //implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // Spigot API
+    compileOnly("org.spigotmc:spigot-api:${mcVersion}-R0.1-SNAPSHOT")
+
+    // PaperMC API
+    compileOnly("io.papermc.paper:paper-api:1.19.2-R0.1-SNAPSHOT")
+}
+
+application{
+    mainClass.set("com.charlotte04.vsccore.VSCCore")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.freeCompilerArgs = listOf("-Xjsr305=strict")
+}
+
+tasks.withType<ShadowJar> {
+
+}
---
+++ src/main/kotlin/com/charlotte04/vsccore/VSCCore.kt
@@ -0,0 +1,31 @@
+package com.charlotte04.vsccore
+
+import net.md_5.bungee.api.ChatColor
+import net.md_5.bungee.api.ChatColor.*
+import org.bukkit.command.CommandExecutor
+import org.bukkit.event.Listener
+import org.bukkit.plugin.Plugin
+import org.bukkit.plugin.java.JavaPlugin
+
+class VSCCore: JavaPlugin() {
+    override fun onEnable() {
+
+        consoleMes("Enabled",GREEN)
+    }
+
+
+    private fun consoleMes(string: String, color: ChatColor){
+        this.server.consoleSender.sendMessage("[VSC_Core]$color$string")
+    }
+
+    private fun regCommand(name: String, executor: CommandExecutor) {
+        getCommand(name)?.run {
+            setExecutor(executor)
+            consoleMes("/$name を登録しました", AQUA)
+        } ?: logger.severe("/$name を登録できませんでした")
+    }
+
+    private fun regEvent(executor: Listener, plugin : Plugin){
+        server.pluginManager.registerEvents(executor, plugin)
+    }
+}
